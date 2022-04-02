285c56aab01a89d2ef3735e2e5a9a0a17f74aa64
Your Name <you@example.com>
2022-04-02 15:55:36 +0700

    a

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,48 +1,63 @@
 import React from "react";
 import "./login.scss";
+import backgournd from "../../assets/images/login/Group341.png";
 import logo from "../../assets/images/login/Logo_alta.png";
 import Button from "../Button/Button";
+import EyeInvisibleOutlined from "@ant-design/icons";
+
 const Login = () => {
   const testfunction = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     console.log("abc");
   };
   return (
-    <div className="login_page">
-      <div className="container">
+    <div className="container">
+      <div className="login_page">
         <div className="login_left">
-          <div className="login_left-wrapper">
+          <div className="login_left-wrap">
             <div className="login_left__logo">
               <img srcSet={`${logo} 2x`} alt="" />
-              <form>
-                <div className="login_left__accountname">
-                  <div className="tittle">Tên đăng nhập *</div>
-                  <input type="text" name="name" placeholder="First Name..." />
-                </div>
+            </div>
+            <form className="login_lef__from">
+              <div className="login_left__account">
+                <span className="login_left__title">Tên đăng nhập *</span>
+                <input
+                  className="login_left__input"
+                  type="text"
+                  name="name"
+                  placeholder="First Name..."
+                />
+              </div>
 
-                <div className="login_left__accountpass">
-                  <div className="tittle">Mật khẩu *</div>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="First Name..."
-                  />
-                  <span className="login_lef__question">Quên mật khẩu</span>
-                </div>
+              <div className="login_left__account">
+                <span className="login_left__title">Mật khẩu *</span>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="First Name..."
+                  className="login_left__input"
+                />
+              </div>
+              <div className="login_left__question">Quên mật khẩu?</div>
+              <div className="login_left__button">
                 <Button
                   handleClick={(event) => {
                     testfunction(event);
                   }}
-                  backgroundColor = "orange"
-
+                  backgroundColor="orange"
                 >
                   Đăng Nhập
                 </Button>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="logim_right"></div>
+        <div
+          className="login_right"
+          style={{
+            backgroundImage: `url(${backgournd})`,
+          }}
+        ></div>
       </div>
     </div>
   );
