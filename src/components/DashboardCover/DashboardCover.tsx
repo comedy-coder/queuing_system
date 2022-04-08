12b8e5c449c289b09187dd3ae116ae9eb8523bff
Your Name <you@example.com>
2022-04-08 16:05:48 +0700

    a

--- src/components/DashboardCover/DashboardCover.tsx
+++ src/components/DashboardCover/DashboardCover.tsx
@@ -4,19 +4,26 @@ import device from "../../assets/images/dashboard/device.png";
 import service from "../../assets/images/dashboard/service.png";
 import level from "../../assets/images/dashboard/level.png";
 import calender from "../../assets/images/dashboard/calenderreal.png";
+import { Link } from "react-router-dom";
 const DashboardCover = () => {
   return (
     <div className="dashboardcover-wrap">
       <span className="dashboardcover-title">Tổng quan</span>
-      <div className="dashboardcover-item">
-        <img srcSet={device} alt="" />
-      </div>
-      <div className="dashboardcover-item">
-        <img srcSet={service} alt="" />
-      </div>
-      <div className="dashboardcover-item">
-        <img srcSet={level} alt="" />
-      </div>
+      <Link to="./device">
+        <div className="dashboardcover-item">
+          <img srcSet={device} alt="" />
+        </div>
+      </Link>
+      <Link to="./dich-vu">
+        <div className="dashboardcover-item">
+          <img srcSet={service} alt="" />
+        </div>
+      </Link>
+      <Link to="./cap-so">
+        <div className="dashboardcover-item">
+          <img srcSet={level} alt="" />
+        </div>
+      </Link>
       <div className="dashboardcover-calender">
         <img srcSet={calender} alt="" />
       </div>
