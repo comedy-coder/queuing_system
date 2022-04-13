5eb24a055d4098852d100368a1924e40fa016e7f
Your Name <you@example.com>
2022-04-13 17:38:13 +0700

    a

--- src/components/Adddevice/Addevice.tsx
+++ src/components/Adddevice/Addevice.tsx
@@ -1,17 +1,29 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import sao from "../../assets/images/addbutton/sao.png";
+import Button from "../Button/Button";
 import "./adddevice.scss";
+import { useNavigate } from "react-router-dom";
 const Adđevice = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/device");
+  };
+  const handleNext = () => {
+    navigate("/device");
+  };
   return (
     <div className="adddevice-wrap">
       <div className="adddevice-title">Quản lý thiết bị</div>
       <div className="adddevice-main">
         <div className="adddevice-main__top">Thông tin thiết bị</div>
         <div className="adddevice-main__group">
-          <Grid container my={1} columnSpacing={4}>
+          <Grid container my={2} columnSpacing={4}>
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Mã thiết bị</div>
+                <div className="adddevice-main__title">
+                  Mã thiết bị : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -21,7 +33,9 @@ const Adđevice = () => {
             </Grid>
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Loại thiết bị</div>
+                <div className="adddevice-main__title">
+                  Loại thiết bị : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -31,7 +45,9 @@ const Adđevice = () => {
             </Grid>
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Tên thiết bị</div>
+                <div className="adddevice-main__title">
+                  Tên thiết bị : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -41,7 +57,9 @@ const Adđevice = () => {
             </Grid>{" "}
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Tên đăng nhập</div>
+                <div className="adddevice-main__title">
+                  Tên đăng nhập : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -51,7 +69,9 @@ const Adđevice = () => {
             </Grid>{" "}
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Địa chỉ IP </div>
+                <div className="adddevice-main__title">
+                  Địa chỉ IP : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -61,7 +81,9 @@ const Adđevice = () => {
             </Grid>{" "}
             <Grid item xs={6}>
               <div className="adddevice-main__info">
-                <div className="adddevice-main__title">Mật khẩu</div>
+                <div className="adddevice-main__title">
+                  Mật khẩu :<img srcSet={`${sao} 2x`} alt="" />{" "}
+                </div>
                 <input
                   type="text"
                   className="adddevice-main__input"
@@ -69,8 +91,37 @@ const Adđevice = () => {
                 />
               </div>
             </Grid>
+            <Grid item xs={12}>
+              <div className="adddevice-main__info">
+                <div className="adddevice-main__title">
+                  Dịch vụ sử dụng : <img srcSet={`${sao} 2x`} alt="" />
+                </div>
+                <input
+                  type="text"
+                  className="adddevice-main__input1"
+                  placeholder="Nhập tài khoản"
+                />
+              </div>
+            </Grid>
           </Grid>
+          <span className="adddevice-main__desc">
+            {" "}
+            <img srcSet={`${sao} 2x`} alt="" /> Là trường thông tin bắt buộc
+          </span>
         </div>
+      </div>
+      <div className="adddevice-main__button">
+        <Button
+          handleClick={handleBack}
+          backgroundColor="orange1"
+          color="orange"
+          border="red"
+        >
+          Hủy bỏ
+        </Button>
+        <Button handleClick={handleNext} backgroundColor="orange" color="white">
+          Thêm thiết bị
+        </Button>
       </div>
     </div>
   );
