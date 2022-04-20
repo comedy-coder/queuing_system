35662648e4a6b6fca1bab70b17114c93894732bb
Your Name <you@example.com>
2022-04-20 21:59:36 +0700

    a

---
+++ src/components/SelectorDropdown/SelectorDD.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./selectordd.scss";
+import arrowdown from "../../assets/images/dashboard/arrow.png";
+import arrowup from "../../assets/images/selector/arrow.png";
+
+export interface IDropDownProps {
+  width: string;
+  title: string;
+  Menu: {
+    display: string;
+    value: string;
+  }[];
+}
+
+const DropDownDD = (props: IDropDownProps) => {
+  const width = { width: `${props.width}` };
+  const title = props.title;
+  const listMenu = props.Menu;
+  const [itemShow, setItemShow] = useState(listMenu[0]);
+  const [isActive, setActive] = useState(false);
+  const itemClick = (index: number) => {
+    setItemShow(listMenu[index]);
+  };
+  const handleClick = () => {
+    setActive(!isActive);
+  };
+
+  return (
+    <div className="dropdown" style={width}>
+      <div className="dropdown__header">
+        <span className="dropdown__header--title">{title}</span>
+      </div>
+      <div className="dropdown__content ">
+        <button
+          className={`dropdown__button  ${isActive ? "active" : ""}`}
+          style={width}
+          onClick={handleClick}
+        >
+          <span className="dropdown__button--title">{itemShow.display}</span>
+          <img
+            srcSet={`${isActive ? arrowup : arrowdown} 2x `}
+            className="dropdown__button--icon"
+            alt="Dropdown"
+          />
+        </button>
+        <div
+          className={`dropdown__list  ${isActive ? "active" : ""}`}
+          style={width}
+        >
+          <div className="dropdown__list--content" style={width}>
+            {listMenu.map((item, index, listMenu) => {
+              return (
+                <button
+                  className="dropdown__list--content-item"
+                  key={index}
+                  onClick={() => itemClick(index)}
+                >
+                  <span className="dropdown__list--content-item-title">
+                    {item.display}
+                  </span>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DropDownDD;
--- src/pages/Devices/Devices.tsx
+++ src/pages/Devices/Devices.tsx
@@ -4,19 +4,33 @@ import Search from "../../components/SearchBox/Search";
 import Selector from "../../components/Selector/Selector";
 import Table from "../../components/Table/Table";
 import "./device.scss";
+import SelectorDD from "../../components/SelectorDropdown/SelectorDD";
 import { Link } from "react-router-dom";
 const Devices = () => {
+  const Active = [
+    {
+      display: "Tất cả",
+      value: "all",
+    },
+    {
+      display: "Hoạt động",
+      value: "active",
+    },
+    {
+      display: "Ngưng hoạt động",
+      value: "deactive",
+    },
+  ];
   const handleChange = () => {};
   return (
     <div className="device-wrap">
       <div className="device-title">Danh sách thiết bị</div>
       <div className="device-selectorgroup">
         <div className="device-selectorgroup1">
-          <Selector
-            title="Trạng thái hoạt động"
-            option1="Tất cả"
-            option2="Hoạt động"
-            option3="Ngưng hoạt động"
+          <SelectorDD
+            width="300px"
+            title="Trang thai hoat dong"
+            Menu={Active}
           />
           <Selector
             title="Trạng thái kết nối"
