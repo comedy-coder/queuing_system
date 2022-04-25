c6e3a90c473f75197dcf99e540726d2c979d1e6e
Your Name <you@example.com>
2022-04-25 22:31:32 +0700

    a

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import calendar from "../../assets/images/addbutton/calendar.png";
 import arrow from "../../assets/images/addbutton/arrow.png";
 import "./calendar.scss";
+import { Box } from "@mui/material";
+import { useState } from "react";
+import { DateRangePicker, DateRange } from "@mui/lab";
 type CalendarProp = {
   styles?: React.CSSProperties;
 };
 const Calendar = ({ styles }: CalendarProp) => {
+  const [value, setValue] = useState<DateRange<Date>>([null, null]);
   return (
     <div className="calendar-wrap" style={styles}>
       <div className="calendar-title">Chọn thời gian</div>
       <div className="calendar-group">
         <button className="calendar-button">
           <img srcSet={`${calendar} 2x`} alt="" />
-          <span>10/10/2021</span>
+          <Box width="50px">
+            <DateRangePicker startText="Check" endText="Check-out" />
+          </Box>
         </button>
         <img srcSet={`${arrow} 2x`} alt="" />
         <button className="calendar-button">
