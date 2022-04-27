249e396682f8bd3afab6caa03bc944e01e14c8b2
Your Name <you@example.com>
2022-04-27 11:43:47 +0700

    a

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -14,6 +14,13 @@ const Calendar = ({ styles }: CalendarProp) => {
     <div className="calendar-wrap" style={styles}>
       <div className="calendar-title">Chọn thời gian</div>
       <div className="calendar-group">
+        <div className="calendar-arrow">
+          <img srcSet={`${arrow} 2x`} alt="" />
+        </div>
+        <div className="calendar-icon">
+          <img srcSet={`${calendar} 2x`} alt="" />
+        </div>
+
         <button className="calendar-button">
           <img srcSet={`${calendar} 2x`} alt="" />
 
